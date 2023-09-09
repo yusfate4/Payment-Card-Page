ab8c24cd301cbb7b17a5212c3bc8a0cf3fc59cfa
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-09-09 18:05:52 +0100

    I updated my js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,98 @@
 // Card number
-$(".input-card-number").on("keyup change", function () {
-  $t = $(this);
-
-  if ($t.val().length > 3) {
-    $t.next().focus();
-  }
-
-  var card_number = " ";
-  $(".input-card-number").each(function () {
-    card_number += $(this).val() + " ";
-    if ($(this).val().length == 4) {
-      $(this).next().focus();
-    }
-  });
-
-  $(".card-container .number").html(card_number);
-});
-
-// Card holder
-$("#card-holder").on("keyup change", function () {
-  $t = $(this);
-  $(".card-container .card-holder div").html($t.val());
-});
-
-// $("#card-holder").on("keyup change", function () {
+// $(".input-card-number").on("keyup change", function () {
 //   $t = $(this);
-//   $(".card-container .card-holder div").html($t.val());
+
+//   if ($t.val().length > 3) {
+//     $t.next().focus();
+//   }
+
+//   var card_number = " ";
+//   $(".input-card-number").each(function () {
+//     card_number += $(this).val() + " ";
+//     if ($(this).val().length == 4) {
+//       $(this).next().focus();
+//     }
+//   });
+
+//   $(".card-container .number").html(card_number);
 // });
 
+const inputFields = document.querySelectorAll("card-digit");
+const card_number = document.getElementById("card-number");
+const card_number_1 = document.getElementById("card-number-1");
+const card_number_2 = document.getElementById("card-number-2");
+const card_number_3 = document.getElementById("card-number-3");
 
-// Card expiration date
+// card number display
+const card_num_display = document.getElementById("card-num-display");
+const card_num_display_1 = document.getElementById("card-num-display-1");
+const card_num_display_2 = document.getElementById("card-num-display-2");
+const card_num_display_3 = document.getElementById("card-num-display-3");
+
+// input 1
+card_number.addEventListener("input", function () {
+  const get_card_num = card_number.value;
+
+  card_num_display.textContent = get_card_num;
+  // console.log(card_num_display);
+});
+
+// input 2
+card_number_1.addEventListener("input", function () {
+  const get_card_num_1 = card_number_1.value;
+
+  card_num_display_1.textContent = get_card_num_1;
+  // console.log(card_num_display);
+});
+
+// input 3
+card_number_2.addEventListener("input", function () {
+  const get_card_num_2 = card_number_2.value;
+
+  card_num_display_2.textContent = get_card_num_2;
+  // console.log(card_num_display);
+});
+
+// input 4
+card_number_3.addEventListener("input", function () {
+  const get_card_num_3 = card_number_3.value;
+
+  card_num_display_3.textContent = get_card_num_3;
+});
+
+// for each input
+inputFields.forEach(function (input, index, fields) {
+  input.addEventListener("input", function () {
+    const get_card_num = input.value;
+    const get_card_num_1 = input.value;
+    const get_card_num_2 = input.value;
+    const get_card_num_3 = input.value;
+
+    if (
+      (get_card_num,
+      get_card_num_1,
+      get_card_num_2,
+      get_card_num_3.length === 4)
+    ) {
+      if (index < fields.length - 1) {
+        fields[index + 1].focus();
+      }
+    }
+  });
+});
+
+// card holder
+const card_holder_name = document.getElementById("card-holder");
+const card_holder_display = document.getElementById("card_holder_display");
+
+card_holder_name.addEventListener("input", function () {
+  const card_holder = card_holder_name.value;
+
+  card_holder_display.textContent = card_holder;
+});
+
+
+// // Card expiration date
 $("#card-expire-month, #card-expire-year").change(function () {
   m = $("#card-expire-month option").index(
     $("#card-expire-month option:selected")
